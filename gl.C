30763841eebe85e23fcafbc86b845cc4c73e9a6c
Xin Qian <xinqian@mac.lan>
2024-12-05 15:55:06 -0500

    update files

---
+++ gl.C
@@ -0,0 +1,53 @@
+#include "TEveManager.h"
+#include "TEveGeoNode.h"
+
+#include "TGeoManager.h"
+#include "TGeoNode.h"
+#include "TGeoVolume.h"
+#include "TGeoMedium.h"
+
+void gl()
+{
+  gSystem->IgnoreSignal(kSigSegmentationViolation, true);
+  TEveManager::Create();
+  // TGeoManager::Import("lbne35t4apa.gdml");
+  //TGeoManager::Import("lbne35t4apa_v3_nowires.gdml");
+  // TGeoManager::Import("lbne35t4apa_v3.gdml");
+  TGeoManager::Import("protodune.gdml");
+
+  TGeoNode* world = gGeoManager->GetTopNode();
+
+  TGeoNode *det = world->GetDaughter(0);
+  // TGeoNode *cryo = det->GetDaughter(0);
+  TEveGeoTopNode* top = new TEveGeoTopNode(gGeoManager, det);
+
+  gEve->AddGlobalElement(top);
+
+
+  // int nDaughters = det->GetNdaughters();
+  // for (int i=0; i<nDaughters; i++) {
+  //   TGeoNode *node = det->GetDaughter(i);
+  //   TString name(node->GetName());
+  //   if (name.Contains("Foam") || name.Contains("Steel") 
+  //       || name.Contains("Concrete") || name.Contains("Neck")) {
+  //     node->SetInvisible();
+  //     node->SetAllInvisible();
+  //   }
+  // }
+
+  // nDaughters = cryo->GetNdaughters();
+  // for (int i=0; i<nDaughters; i++) {
+  //   TGeoNode *node = cryo->GetDaughter(i);
+  //   TString name(node->GetName());
+  //   if (name.Contains("TPC") || name.Contains("Steel") 
+  //       || name.Contains("Concrete") || name.Contains("Argon")) {
+  //     node->SetInvisible();
+  //     node->SetAllInvisible();
+  //   }
+  // }
+
+  // // cryo->GrabFocus();
+
+  gEve->Redraw3D(kTRUE);
+
+}
